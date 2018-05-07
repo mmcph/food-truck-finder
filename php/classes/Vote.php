896dc78c5fb3bee427c51fa76a702400c3dd3b37
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-05-07 15:59:06 -0600

    created Vote.php class file

--- php/classes/Vote.php
+++ php/classes/Vote.php
@@ -1,1 +1,15 @@
 <?php
+namespace Edu\Cnm\FoodTruck;
+
+require_once("autoload.php");
+require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
+
+use Ramsey\Uuid\Uuid;
+
+/**
+ * This is the class for our attribute of Vote
+ *
+ *
+ * @author Yvette Johnson-Rodgers <itsyvejr@gmail.com>
+ * @version 1.0
+ **/
