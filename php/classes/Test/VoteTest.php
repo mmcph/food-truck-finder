b100d0416dbe38f5b42a8ef78eeba6b9903e783f
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-05-11 16:45:11 -0600

    cleared out the template (incorrect) and began writing the class VoteTest https://app.asana.com/0/656403034581809/660901941582676

--- php/classes/Test/VoteTest.php
+++ php/classes/Test/VoteTest.php
@@ -1,106 +1,43 @@
 <?php
-namespace Edu\Cnm\DataDesign\Test;
+namespace Edu\Cnm\TacoTruck\Test
 
-use PHPUnit\Framework\TestCase;
-use PHPUnit\DbUnit\TestCaseTrait;
-use PHPUnit\DbUnit\DataSet\QueryDataSet;
-use PHPUnit\DbUnit\Database\Connection;
-use PHPUnit\DbUnit\Operation\{Composite, Factory, Operation};
+use Edu\Cnm\FoodTruck\{Vote, Profile, Truck};
 
-// grab the encrypted properties file
-require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+// grab the class under scrutiny
+require_once(dirname(__DIR__) . "/autoload.php");
 
-// autoload Composer packages
-require_once(dirname(__DIR__, 3) . "/vendor/autoload.php");
+// grab the uuid generator
+require_once(dirnam(__DIR__, 2) . "lib/uuid.php");
 
 /**
- * Abstract class containing universal and project specific mySQL parameters
+ * Full PHPUnit test for the Vote class
  *
- * This class is designed to lay the foundation of the unit tests per project. It loads the all the database
- * parameters about the project so that table specific tests can share the parameters in on place. To use it:
+ * This is a complete PHPUnit test of the Vote class. It is complete because it tests all mySQL/PDO enabled methods
+ * for both valid and invalid inputs
  *
- * 1. Rename the class from DataDesignTest to a project specific name (e.g., ProjectNameTest)
- * 2. Rename the namespace to be the same as (1) (e.g., Edu\Cnm\ProjectName\Test)
- * 3. Modify DataDesignTest::getDataSet() to include all the tables in your project.
- * 4. Modify DataDesignTest::getConnection() to include the correct mySQL properties file.
- * 5. Have all table specific tests include this class.
- *
- * *NOTE*: Tables must be added in the order they were created in step (2).
- *
+ * @see Vote
  * @author Dylan McDonald <dmcdonald21@cnm.edu>
+ *
  **/
-abstract class DataDesignTest extends TestCase {
 
-    use TestCaseTrait;
-
+class VoteTest extends TacoTruckTest {
     /**
-     * PHPUnit database connection interface
-     * @var Connection $connection
+     * Profile that created the Vote; this is for foreign key relations
+     * @var Vote $vote
      **/
-    protected $connection = null;
+
+    private $profile;
 
     /**
-     * assembles the table from the schema and provides it to PHPUnit
      *
-     * @return QueryDataSet assembled schema for PHPUnit
-     **/
-    public final function getDataSet() {
-        $dataset = new QueryDataSet($this->getConnection());
-
-        // add all the tables for the project here
-        // THESE TABLES *MUST* BE LISTED IN THE SAME ORDER THEY WERE CREATED!!!!
-        $dataset->addTable("profile");
-        $dataset->addTable("tweet");
-        $dataset->addTable("like");
-        return($dataset);
-    }
-
+     */
+    private $truck;
     /**
-     * templates the setUp method that runs before each test; this method expunges the database before each run
      *
-     * @see https://phpunit.de/manual/current/en/fixtures.html#fixtures.more-setup-than-teardown PHPUnit Fixtures: setUp and tearDown
-     * @see https://github.com/sebastianbergmann/dbunit/issues/37 TRUNCATE fails on tables which have foreign key constraints
-     * @return Composite array containing delete and insert commands
      **/
-    public final function getSetUpOperation() {
-        return new Composite([
-            Factory::DELETE_ALL(),
-            Factory::INSERT()
-        ]);
-    }
-
+    private $;
     /**
-     * templates the tearDown method that runs after each test; this method expunges the database after each run
      *
-     * @return Operation delete command for the database
      **/
-    public final function getTearDownOperation() {
-        return(Factory::DELETE_ALL());
-    }
 
-    /**
-     * sets up the database connection and provides it to PHPUnit
-     *
-     * @see <https://phpunit.de/manual/current/en/database.html#database.configuration-of-a-phpunit-database-testcase>
-     * @return Connection PHPUnit database connection interface
-     **/
-    public final function getConnection() {
-        // if the connection hasn't been established, create it
-        if($this->connection === null) {
-            // connect to mySQL and provide the interface to PHPUnit
-            $config = readConfig("/etc/apache2/capstone-mysql/dmcdonald21.ini");
-            $pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/dmcdonald21.ini");
-            $this->connection = $this->createDefaultDBConnection($pdo, $config["database"]);
-        }
-        return($this->connection);
-    }
-
-    /**
-     * returns the actual PDO object; this is a convenience method
-     *
-     * @return \PDO active PDO object
-     **/
-    public final function getPDO() {
-        return($this->getConnection()->getConnection());
-    }
 }
