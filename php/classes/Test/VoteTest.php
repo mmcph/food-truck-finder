1377838105e0cc681f05dea73cc5f6f83c167b51
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-05-12 11:08:04 -0600

    completed class extends section of VoteTest https://app.asana.com/0/656403034581809/660901941582676

--- php/classes/Test/VoteTest.php
+++ php/classes/Test/VoteTest.php
@@ -2,6 +2,7 @@
 namespace Edu\Cnm\TacoTruck\Test
 
 use Edu\Cnm\FoodTruck\{Vote, Profile, Truck};
+use function Sodium\randombytes_random16;
 
 // grab the class under scrutiny
 require_once(dirname(__DIR__) . "/autoload.php");
@@ -25,19 +26,37 @@ class VoteTest extends TacoTruckTest {
      * Profile that created the Vote; this is for foreign key relations
      * @var Vote $vote
      **/
-
     private $profile;
-
     /**
-     *
+     * this is the truck being voted on
      */
     private $truck;
     /**
-     *
+     * valid hash to use
      **/
-    private $;
+    private $VALID_HASH;
+    /**
+     * valid activationToken to create the profile object to own the test
+     * @var string $VALID_ACTIVATION
+     **/
+    private $VALID_ACTIVATION;
+
+
     /**
      *
-     **/
+     *
+     */
+    public final function setUp() : void {
+        // run the default setUp() method first
+        parent::setUp();
 
+        // create a hash for the mocked profile
+        $password = "abc123";
+        $this->VALID_HASH = password_hash($password, PASSWORD_ARGON2I, ["time_cost" => 384]);
+        $this->VALID_ACTIVATION = bin2hex(random_bytes(16));
+
+        // create and insert the mocked profile
+        $this->profile = new Profile(generateUuid4(), null, "@phpunit", "https")
+
+    }
 }
