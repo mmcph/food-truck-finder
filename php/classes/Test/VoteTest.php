0ecbdea6f738d97ba83323b2b358e4495bd8576d
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-05-14 14:40:28 -0600

    created mock truck. all of setup function for VoteTest completed. https://app.asana.com/0/656403034581809/660901941582676

--- php/classes/Test/VoteTest.php
+++ php/classes/Test/VoteTest.php
@@ -53,18 +53,17 @@ class VoteTest extends TacoTruckTest {
         $this->VALID_HASH = password_hash($password, PASSWORD_ARGON2I, ["time_cost" => 384]);
         $this->VALID_ACTIVATION = bin2hex(random_bytes(16));
 
-        // create and insert the mocked profile
-        $this->profile = new Profile (generateUuid4(), "test@phpunit.de", $this->VALID_HASH, 1, "php", "unit", "phpunit");
+        // create and insert the mocked profile into mySQL
+        $this->profile = new Profile (generateUuidV4(), "test@phpunit.de", $this->VALID_HASH, 1, "php", "unit", "phpunit");
         $this->profile->insert($this->getPDO());
 
-
-
-    //todo finish writing mocked truck
         //create and insert the mocked truck
-        $this->truck = new Truck(generateUuid4(), $this->truck->getTruckId(), "????")
+        $this->truck = new Truck (generateUuidV4(), $this->profile->getProfileId(),"I am a happy little truck.", 1, 35.0772, 106.6614, "LegenDairy", 5058596496, "https://phpunit.de/");
+        $this->truck->insert($this->getPDO());
+
 
         // create and insert the mocked vote
-        $this->vote = new Vote(generateUuid4(), $this->profile->getProfileId(), "PHPUnit vote test passing");
+        $this->vote = new Vote(generateUuidV4(), $this->profile->getProfileId(), "PHPUnit vote test passing");
         $this->vote->insert($this->getPDO());
     }
     /**
