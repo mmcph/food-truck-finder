c60442c9e782325894109c2cce325c848daa9794
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-05-14 21:25:45 -0600

    wrote getVoteByVoteProfileIdAndVoteTruckId// https://app.asana.com/0/656403034581809/660901941582676

--- php/classes/Test/VoteTest.php
+++ php/classes/Test/VoteTest.php
@@ -1,15 +1,11 @@
 <?php
 namespace Edu\Cnm\FoodTruck\Test;
-
 use Edu\Cnm\FoodTruck\{Vote, Profile, Truck};
 use function Sodium\randombytes_random16;
-
 // grab the class under scrutiny
 require_once(dirname(__DIR__) . "/autoload.php");
-
 // grab the uuid generator
 require_once(dirname(__DIR__, 2) . "lib/uuid.php");
-
 /**
  * Full PHPUnit test for the Vote class
  *
@@ -20,7 +16,6 @@ require_once(dirname(__DIR__, 2) . "lib/uuid.php");
  * @author Dylan McDonald <dmcdonald21@cnm.edu>
  *
  **/
-
 class VoteTest extends TacoTruckTest {
     /**
      * Profile that created the Vote; this is for foreign key relations
@@ -47,21 +42,16 @@ class VoteTest extends TacoTruckTest {
     public final function setUp() : void {
         // run the default setUp() method first
         parent::setUp();
-
         // create a hash for the mocked profile
         $password = "abc123";
         $this->VALID_HASH = password_hash($password, PASSWORD_ARGON2I, ["time_cost" => 384]);
         $this->VALID_ACTIVATION = bin2hex(random_bytes(16));
-
         // create and insert the mocked profile into mySQL
         $this->profile = new Profile (generateUuidV4(), "test@phpunit.de", $this->VALID_HASH, 1, "php", "unit", "phpunit");
         $this->profile->insert($this->getPDO());
-
         //create and insert the mocked truck
         $this->truck = new Truck (generateUuidV4(), $this->profile->getProfileId(),"I am a happy little truck.", 1, 35.0772, 106.6614, "LegenDairy", 5058596496, "https://phpunit.de/");
         $this->truck->insert($this->getPDO());
-
-
         // create and insert the mocked vote
         $this->vote = new Vote(generateUuidV4(), $this->profile->getProfileId(), "PHPUnit vote test passing");
         $this->vote->insert($this->getPDO());
@@ -102,15 +92,35 @@ class VoteTest extends TacoTruckTest {
     $pdoVote = Vote::getVoteByVoteProfileIdAndVoteTruckId($this->getPDO(),$this->profile->getProfileId(),$this->truck->getTruckId());
     $this->assertNull($pdoVote);
     $this->assertEquals($numRows, $this->getConnection()->getRowCount("vote"));
-
     }
-
     /**
      * test inserting a Vote and re-grabbing it from mySQL
      */
-    public function testGetValidVoteByProfileIdAndTruckId () {
 
+
+
+
+
+    public function testGetValidVoteByProfileIdAndTruckId () {
+        // count the number of rows and save it for later
+        $numRows = $this->getConnection()->getRowCount("vote");
+        // create a new Vote and insert into my SQL
+        $vote = new Vote($this->profile->getProfileId(), $this->truck->getTruckId(), $this->voteValue->getVoteValue());
+        $vote->insert($this->getPDO());
+        // grab the data from mySQL and enforce the fields match our expectations
+        $pdoVote = Vote::getVoteByVoteProfileIdAndVoteTruckId($this->getPDO(),$this->profile->getProfileId(),$this->vote->getTruckId());
+        $this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("vote"));
+        $this->assertEquals($pdoVote->getVoteProfileId(),$this->profile->getProfileId());
+        $this->assertEquals($pdoVote->getVoteTruckId(),
+            $this->vote->getTruckId());
+
+        //todo question - are we testing the vote values anywhere?
     }
+
+
+
+
+
 
     /**
      * test grabbing a Vote that does not exist
