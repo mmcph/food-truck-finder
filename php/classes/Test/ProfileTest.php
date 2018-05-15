5b8d4e4b2a092dc2db4fdea0888044b466fc2b7c
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-05-15 14:02:05 -0600

    wrote ProfileTest extends TacoTruckTest https://app.asana.com/0/656403034581732/660901941582672

--- php/classes/Test/ProfileTest.php
+++ php/classes/Test/ProfileTest.php
@@ -1,18 +1,101 @@
 <?php
 namespace Edu\Cnm\FoodTruck\Test;
-use Edu\Cnm\FoodTruck\{Vote, Profile, Truck};
-use function Sodium\randombytes_random16;
+use Edu\Cnm\FoodTruck\Profile;
+
 // grab the class under scrutiny
 require_once(dirname(__DIR__) . "/autoload.php");
+
 // grab the uuid generator
 require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
+
 /**
- * Full PHPUnit test for the Vote class
+ * Full PHPUnit test for the Profile class
  *
- * This is a complete PHPUnit test of the Vote class. It is complete because it tests all mySQL/PDO enabled methods
+ * This is a complete PHPUnit test of the Profile class. It is complete because it tests all mySQL/PDO enabled methods
  * for both valid and invalid inputs
  *
- * @see Vote
+ * @see Profile
  * @author Dylan McDonald <dmcdonald21@cnm.edu>
  *
  **/
+class ProfileTest extends TacoTruckTest {
+    /*
+     * placeholder until account activation is created
+     * @var string $VALID_ACTIVATION
+     **/
+    private $VALID_ACTIVATION;
+    /**
+     * valid email to use
+     * @var string $VALID_EMAIL
+     */
+    private $VALID_EMAIL = "test@phpunit.de";
+    /**
+     * valid hash to use
+     * @var $VALID_HASH
+     **/
+    private $VALID_HASH;
+    /**
+     * @var int $VALID_ISOWNER
+     **/
+    private $VALID_ISOWNER = 1 ;
+    /**
+     * valid first name to use
+     * @var $VALID_FIRSTNAME
+     **/
+    private $VALID_FIRSTNAME = "Marika";
+    /**
+     * valid last name to use
+     * @var string $VALID_LASTNAME
+     **/
+    private $VALID_LASTNAME = "Johnson";
+    /**
+     * valid username to use
+     * @var string $VALID_USERNAME
+     **/
+    private $VALID_USERNAME = "MarikaJ";
+
+
+
+
+
+    /**
+     *
+     **/
+    public final function setUp() : void {
+    parent::setUp();
+    //
+    $password = "strongpassword";
+    --------------------------------------
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
