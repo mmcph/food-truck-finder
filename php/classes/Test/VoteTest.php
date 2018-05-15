9f46ce7d1f6dd84ecbe7b4a5fcde373cd4bba1d7
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-05-15 09:42:56 -0600

    corrected pathing for the lib directory inside votetest.php // https://app.asana.com/0/656403034581809/660901941582676

--- php/classes/Test/VoteTest.php
+++ php/classes/Test/VoteTest.php
@@ -5,7 +5,7 @@ use function Sodium\randombytes_random16;
 // grab the class under scrutiny
 require_once(dirname(__DIR__) . "/autoload.php");
 // grab the uuid generator
-require_once(dirname(__DIR__, 2) . "lib/uuid.php");
+require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
 /**
  * Full PHPUnit test for the Vote class
  *
