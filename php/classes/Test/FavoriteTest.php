1d48762ea13de4b2f53241e171ed840bd15ab936
Marlon McPherson <marlon.c.mcpherson@gmail.com>
2018-05-16 10:52:47 -0600

    began work on FavoriteTest https://app.asana.com/0/660901941582670/660901941582678

---
+++ php/classes/Test/FavoriteTest.php
@@ -0,0 +1,35 @@
+<?php
+namespace Edu\Cnm\FoodTruck\Test;
+
+use Edu\Cnm\FoodTruck\{Profile, Truck};
+
+// grab the class under scrutiny
+require_once(dirname(__DIR__) . "/autoload.php");
+
+// grab the uuid generator
+require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
+
+/**
+ * Full PHPUnit test for the Favorite class
+ *
+ * This is a complete PHPUnit test of the Favorite class. It is complete because *ALL* mySQL/PDO enabled methods
+ * are tested for both invalid and valid inputs.
+ *
+ * @see Favorite
+ * @author Marlon McPherson <mmcpherson5@cnm.edu>
+ * @author
+ **/
+class FavoriteTest extends TacoTruckTest {
+	/**
+	 * Profile that created the Favorite; this is for foreign key relations
+	 * @var Profile profile
+	 **/
+	protected $profile = null;
+	/**
+	 * Truck that is favorited; this is for foreign key relations
+	 * var Truck truck
+	 **/
+	protected $truck = null;
+
+
+}
