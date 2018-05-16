c7314107ee85c9c7b556d825ce4b7afc6a274557
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-05-16 11:30:31 -0600

    wrote setup for favoriteTest https://app.asana.com/0/656403034581732/660901941582678

--- php/classes/Test/FavoriteTest.php
+++ php/classes/Test/FavoriteTest.php
@@ -21,14 +21,44 @@ require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
 class FavoriteTest extends TacoTruckTest {
 	/**
 	 * Profile that created the Favorite; this is for foreign key relations
-	 * @var Profile
+	 * @var Profile $profile
 	 **/
-	protected $profile = null;
+	protected $profile;
 	/**
 	 * Truck that is favorited; this is for foreign key relations
-	 * var Truck
+	 * @var Truck $truck
 	 **/
-	protected $truck = null;
-
-
+	protected $truck;
+    /**
+     * valid hash to use
+     * @var $VALID_HASH
+     **/
+    private $VALID_HASH;
+    /**
+     * valid activationToken to create the profile object to own the test
+     * @var string $VALID_ACTIVATION
+     **/
+    private $VALID_ACTIVATION;
+    /**
+     *
+     * create dependent objects before running each test
+     */
+    public final function setUp() : void {
+        // run the default setUp() method first
+        parent::setUp();
+        // create a hash for the mocked profile
+        $password = "abc123";
+        $this->VALID_HASH = password_hash($password, PASSWORD_ARGON2I, ["time_cost" => 384]);
+        $this->VALID_ACTIVATION = bin2hex(random_bytes(16));
+        // create and insert the mocked profile into mySQL
+        $this->profile = new Profile (generateUuidV4(), "test@phpunit.de", $this->VALID_HASH, 1, "php", "unit", "phpunit");
+        $this->profile->insert($this->getPDO());
+        //create and insert the mocked truck
+        $this->truck = new Truck (generateUuidV4(), $this->profile->getProfileId(), "I am a happy little truck.", 1, 35.0772, 106.6614, "LegenDairy", 5058596496, "https://phpunit.de/");
+        $this->truck->insert($this->getPDO());
+        // create and insert the mocked vote
+        $this->vote = new Vote(generateUuidV4(), $this->profile->getProfileId(), 1);
+        $this->vote->insert($this->getPDO());
+    }
 }
+
