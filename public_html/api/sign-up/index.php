0f0514c6bb33e1dc32e64a5fea097194281bad3a
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-05-23 14:35:34 -0600

    completed the signup api - pending scrub https://app.asana.com/0/656403034581809/683120231545465

--- public_html/api/sign-up/index.php
+++ public_html/api/sign-up/index.php
@@ -85,10 +85,17 @@ try {
         //insert the profile into the database
         $profile->insert($pdo);
 
-        //compose the email message to send with th activation token
+        //compose the email message to send with the activation token
         $messageSubject = "Please check your email to complete your account activation.";
 
-        //make sure URL is /public_html/api/        //building the activation link that can travel to another server and still work. This is the link that will be clicked to confirm the account.activation/$activation
+
+//----------------------------------------------------
+
+/**
+ * building the activation link that can travel to another server and still work.
+ * This is the link that will be clicked to confirm the account.
+ * make sure URL is /public_html/api/activation/$activation
+ */
         $basePath = dirname($_SERVER["SCRIPT_NAME"], 3);
         //create the path
         $urlglue = $basePath . "/api/activation/?activation=" . $profileActivationToken;
@@ -96,7 +103,67 @@ try {
         $confirmLink = "https://" . $_SERVER["SERVER_NAME"] . $urlglue;
         //compose message to send with email
         $message = <<< EOF
-
-} else {
-    throw (new InvalidArgumentException("invalid http request"));
+<h2>Welcome to FoodTruck Finder.</h2>
+<p>In order to complete registration, you must confirm your account </p>
+<p><a href="$confirmLink">$confirmLink</a></p>
 EOF;
+        //create swift email
+        $swiftMessage = new Swift_Message();
+        // attach the sender to the message
+        // this takes the form of an associative array where the email is the key to a real name
+        $swiftMessage->setFrom(["yjohnson6@cnm.edu" => "Yjohnson"]);
+        /**
+         * attach recipients to the message
+         * notice this is an array that can include or omit the recipient's name
+         * use the recipient's real name where possible;
+         * this reduces the probability of the email is marked as spam
+         */
+        //define who the recipient is
+        $recipients = [$requestObject->profileEmail];
+        //set the recipient to the swift message
+        $swiftMessage->setTo($recipients);
+        //attach the subject line to the email message
+        $swiftMessage->setSubject($messageSubject);
+        /**
+         * attach the message to the email
+         * set two versions of the message: a html formatted version and a filter_var()ed version of the message, plain text
+         * notice the tactic used is to display the entire $confirmLink to plain text
+         * this lets users who are not viewing the html content to still access the link
+         */
+        //attach the html version fo the message
+        $swiftMessage->setBody($message, "text/html");
+        //attach the plain text version of the message
+        $swiftMessage->addPart(html_entity_decode($message), "text/plain");
+        /**
+         * send the Email via SMTP; the SMTP server here is configured to relay everything upstream via CNM
+         * this default may or may not be available on all web hosts; consult their documentation/support for details
+         * SwiftMailer supports many different transport methods; SMTP was chosen because it's the most compatible and has the best error handling
+         * @see http://swiftmailer.org/docs/sending.html Sending Messages - Documentation - SwitftMailer
+         **/
+        //setup smtp
+        $smtp = new Swift_SmtpTransport(
+            "localhost", 25);
+        $mailer = new Swift_Mailer($smtp);
+        //send the message
+        $numSent = $mailer->send($swiftMessage, $failedRecipients);
+        var_dump($numSent);
+        /**
+         * the send method returns the number of recipients that accepted the Email
+         * so, if the number attempted is not the number accepted, this is an Exception
+         **/
+        if($numSent !== count($recipients)) {
+            // the $failedRecipients parameter passed in the send() method now contains contains an array of the Emails that failed
+            throw(new RuntimeException("unable to send email", 400));
+        }
+        // update reply
+        $reply->message = "Thank you for creating a profile with DDC-Twitter";
+    } else {
+        throw (new InvalidArgumentException("invalid http request"));
+    }
+} catch(\Exception |\TypeError $exception) {
+    $reply->status = $exception->getCode();
+    $reply->message = $exception->getMessage();
+    $reply->trace = $exception->getTraceAsString();
+}
+header("Content-type: application/json");
+echo json_encode($reply);
