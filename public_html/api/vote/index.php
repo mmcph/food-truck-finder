d7297cae65c8033ef16edc90d788aa31410de1ba
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-05-24 13:46:56 -0600

    added setup to vote api https://app.asana.com/0/656403034581809/683120231545460

---
+++ public_html/api/vote/index.php
@@ -0,0 +1,46 @@
+<?php
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use Edu\Cnm\Foodtruck\{
+    Vote,
+    Profile,
+    Truck
+};
+
+
+/**
+ * api for the Tweet class
+ *
+ * @author <itsyvejr@gmail.com>
+ **/
+
+//verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+    //grab the mySQL connection
+    $pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/foodtruck.ini");
+
+    //determine which HTTP method was used
+    $method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
+
+    //sanitize input
+    $voteProfileId = filter_input(INPUT_GET, "voteProfileId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+    $voteTruckId = filter_input(INPUT_GET, "voteTruckId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+    $voteValue = filter_input(INPUT_GET, "voteValue", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+//todo is this the best sanitization for voteValue int?
+    //make sure the id is valid for methods that require it
+    if(($method === "DELETE" || $method === "PUT") && (empty($id) === true)) {
+        throw(new InvalidArgumentException("id cannot be empty or negative", 405));
+    }
