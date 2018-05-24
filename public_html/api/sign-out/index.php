a4b5c28b599267e25a6157b081bef6469346b9ee
Marlon McPherson <marlon.c.mcpherson@gmail.com>
2018-05-24 13:47:32 -0600

    Customized sign-out API to reflect project details https://app.asana.com/0/660901941582670/683120231545464

--- public_html/api/sign-out/index.php
+++ public_html/api/sign-out/index.php
@@ -6,7 +6,7 @@ require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
  * api for signing out
  *
  * @author Gkephart
- * @version 1.0
+ * @editor MMCPH
  **/
 //verify the xsrf challenge
 if(session_status() !== PHP_SESSION_ACTIVE){
@@ -18,7 +18,7 @@ $reply->status = 200;
 $reply->data = null;
 try {
 	//grab the mySQL connection
-	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/ddctwitter.ini");
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/foodtruck.ini");
 	//determine which HTTP method was used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 	if($method === "GET") {
