2f55746f9054e131a0d56447da41890785e33c2c
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-05-29 16:34:21 -0600

    removed var dump in sign-up api

--- public_html/api/sign-up/index.php
+++ public_html/api/sign-up/index.php
@@ -146,7 +146,7 @@ EOF;
         $mailer = new Swift_Mailer($smtp);
         //send the message
         $numSent = $mailer->send($swiftMessage, $failedRecipients);
-        var_dump($numSent);
+
         /**
          * the send method returns the number of recipients that accepted the Email
          * so, if the number attempted is not the number accepted, this is an Exception
