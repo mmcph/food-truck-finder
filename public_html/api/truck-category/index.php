af7483ebf283cbcc30cb7bc59f23be6a2f22ba06
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-05-29 16:02:51 -0600

    removed un-needed method and cleaned up error messages https://app.asana.com/0/660901941582670/683120231545461

--- public_html/api/truck-category/index.php
+++ public_html/api/truck-category/index.php
@@ -5,11 +5,7 @@ require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
-use Edu\Cnm\FoodTruck\{
-	TruckCategory,
-	Category,
-	Truck
-};
+use Edu\Cnm\FoodTruck\TruckCategory;
 
 
 /**
@@ -31,61 +27,45 @@ $reply->status = 200;
 $reply->data = null;
 
 try {
-	//grab the mySQL connection
+//	//grab the mySQL connection
 	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/foodtruck.ini");
-
-	//determine which HTTP method was used
+//
+//	//determine which HTTP method was used
 	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
 
-	//sanitize input
-	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-
-	$truckCategoryTruckId = filter_input(INPUT_GET, "truckCategoryTruckId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-
-	//make sure the id is valid for methods that require it
+//make sure the id is valid for methods that require it
 	if(($method === "DELETE" || $method === "POST") && (empty($id) === true)) {
 		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
-	}
+	} if($method === "POST") {
 
+        // enforce the user has a XSRF token
+        verifyXsrf();
 
+        //  Retrieves the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input") to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string. The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read from the front end request which is, in this case, a JSON package.
+        $requestContent = file_get_contents("php://input");
 
+        // This Line Then decodes the JSON package and stores that result in $requestObject
+        $requestObject = json_decode($requestContent);
 
+        //make sure Truck Category is available (required field)
+        if (empty($requestObject->truckCategoryCategoryId) === true) {
+            throw(new \InvalidArgumentException ("Category ID does not exist.", 405));
 
-
-
-
-
-	else if($method === "POST") {
-
-		// enforce the user has a XSRF token
-		verifyXsrf();
-
-		//  Retrieves the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input") to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string. The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read from the front end request which is, in this case, a JSON package.
-		$requestContent = file_get_contents("php://input");
-
-		// This Line Then decodes the JSON package and stores that result in $requestObject
-		$requestObject = json_decode($requestContent);
-
-		//make sure Truck Category is available (required field)
-		if(empty($requestObject->TruckCategory) === true) {
-			throw(new \InvalidArgumentException ("No content for TruckCategory.", 405));
-		}
-
-		//  make sure truckId is available
-		if(empty($requestObject->truckId) === true) {
-			throw(new \InvalidArgumentException ("No Truck ID.", 405));
+        //  make sure truckId is available
+        } if(empty($requestObject->truckCategoryTruckId) === true) {
+			throw(new \InvalidArgumentException ("Truck ID does not exist.", 405));
 		}
 
 		} else if($method === "POST") {
 
 			// enforce the user is signed in
 			if(empty($_SESSION["profile"]) === true) {
-				throw(new \InvalidArgumentException("You must be logged in to post Truck Category's", 403));
+				throw(new \InvalidArgumentException("You must be logged in to post to Truck Category", 403));
 			}
 
-			// create new Profile and insert into the database
-		$profile = new Profile(generateUuidV4(), $_SESSION["profile"]->getProfileId, $requestObject->truckCategory, null);
-		$profile->insert($pdo);
+			// create new truck category and insert into the database
+		$truckCategory = new TruckCategory($requestObject->truckCategoryCategoryId, $requestObject->truckCategoryTruckId);
+		$truckCategory->insert($pdo);
 
 			// update reply
 			$reply->message = "Truck category created OK";
