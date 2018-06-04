22cc3c4e04bfe2e513c3c01517bdd5c2c73afa3f
G Cordova <gcordovaone@gmail.com>
2018-06-04 11:25:01 -0600

    fixing system issues w/ George https://app.asana.com/0/660901941582670/683120231545459

--- public_html/api/category/index.php
+++ public_html/api/category/index.php
@@ -7,6 +7,7 @@ require_once(dirname(__DIR__, 3) . "/php/lib/uuid.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 use Edu\Cnm\FoodTruck\Category;
 
+
 /**
  * API for category
  *
