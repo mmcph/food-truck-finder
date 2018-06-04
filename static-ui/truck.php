28218495e9664461b8aa845792934fedbc99e4fe
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-06-04 13:53:16 -0600

    created structure for the truck info page

---
+++ static-ui/truck.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Font Awesome CDN for icons-->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>
+
+    <!-- Custom CS link -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- jQuery, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <!--Google Fonts-->
+
+    <!--Actual page begins-->
+    <title>Truck Details</title>
+</head>
+<body>
+<!-- Header -->
+    <h1><i class="far fa-star"></i> Mario's Super Truck</h1>
+<!--Bio-->
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <img src="images/mario_thumbnail.png" class="img-fluid">
+            </div>
+            <div class="col-sm-6">
+                <p>We serve the best pizza in Albuquerque, and also the best cannolis!</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <button type="button" class="btn btn-primary">Menu</button>
+            </div>
+            <div class="col-sm-6">
+                <p>Categories</p>
+            </div>
+        </div>
+    </div>
+</body>
