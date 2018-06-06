3aa95357b53def38564177f2b17f2c83e24a44b4
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-06-06 14:25:29 -0600

    truck.service.ts -- wrote POST, PUT, DELETE, and GET by id

---
+++ src/app/service/truck.services.ts
@@ -0,0 +1,52 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+
+import {Status} from "../classes/status";
+import {Profile} from "..classes/truck";
+import {Observable} from "rxjs/Observable";
+
+@Injectable()
+export class ProfileService {
+
+    constructor(protected http: HttpClient) {
+
+    }
+
+    //define the API endpoint
+    private truckUrl = "api/truck/";
+
+
+    //reach out to the Truck API and create the truck (POST)
+    createTruck(truck : Truck) : Observable<Status> {
+        return(this.http.post<Status>(this.truckUrl, truck));
+    }
+
+    // call to the Truck API to grab the truck in question and edit it (PUT)
+    editTruck(truck : Truck) : Observable<Status> {
+        return(this.http.put<Status>(this.truckUrl + truck.truckId, truck));
+    }
+
+    //reach out to the Truck API and delete the truck in question (DELETE)
+    deleteTruck(truckId : string) : Observable<Status> {
+        return(this.http.delete<Status>(this.truckUrl + id));
+    }
+
+    // call to the Truck API and get a truck object by its truckId (GET)
+    getTruck(id : string) : Observable<Truck> {
+        return(this.http.get<Truck>(this.truckUrl + id));
+    }
+
+    // call to the Truck API to ----
+
+
+    // call to the Truck API and grab the corresponding truck by its _____? (GET array)
+
+
+    //  (GET array)
+
+
+
+
+
+
+}
