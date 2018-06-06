ac4c029620466976227f4c604fbbb36e4a1bf5df
G Cordova <gcordovaone@gmail.com>
2018-06-06 14:58:12 -0600

    created favorite service (no asana ticket yet)

---
+++ src/app/shared/services/favorite.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Status} from "../classes/status";
+import {Favorite} from "../classes/favorite";
+import {Observable} from "rxjs/Observable";
+
+@Injectable ()
+export class FavoriteService {
+
+	constructor(protected http : HttpClient ) {
+
+	}
+
+	//define the API endpoint
+	private favoriteUrl = "/api/favorite/";
+
+
+	//call the like API and create a new like
+	createFavorite(favorite : Favorite) : Observable<Status> {
+		return (this.http.post<Status>(this.favoriteUrl, favorite));
+	}
+
+	//grabs a  favorite based on its composite key
+	getFavoriteByCompositeKey(favoriteProfileId : string, favoriteTruckId : string) : Observable <Favorite> {
+		return (this.http.get<Favorite>(this.favoriteUrl+ "?favoriteProfileId=" + favoriteProfileId +"&favoriteTruckId=" + favoriteTruckId))
+	}
+
+	getFavoriteBytruckId (likeTruckId : string) : Observable <Favorite[]> {
+		return(this.http.get<Favorite[]>(this.favoriteUrl + favoriteTruckId))
+	}
+
+
+}
