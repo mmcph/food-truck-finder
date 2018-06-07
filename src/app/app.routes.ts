6d961fede8bf93af836a5e86a417d38e5a09d5f0
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-06-07 15:20:19 -0600

    added interceptor to app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./home/home.component";
 import {APP_BASE_HREF} from "@angular/common";
 import {SignUpComponent} from "./sign-up/sign-up.component";
+import {DeepDiveInterceptor} from "./shared/interceptors/deep.dive.interceptor";
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
 
 
 export const allAppComponents = [HomeComponent];
@@ -13,7 +15,8 @@ export const routes: Routes = [
 
 export const appRoutingProviders: any[] = [
     {provide: APP_BASE_HREF, useValue: window["_base_href"]},
-    {}
+    {provide: HTTP_INTERCEPTORS, useClass: DeepDiveInterceptor, multi: true}
+
 ];
 
 export const routing = RouterModule.forRoot(routes);
