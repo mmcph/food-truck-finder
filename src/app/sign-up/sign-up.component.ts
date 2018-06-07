23f67b0246120746b5c73f766754cd378be2d6a5
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-06-07 14:54:14 -0600

    fixed all the errors

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,39 +1,22 @@
-import {Component, OnInit, ViewChild,} from "@angular/core";
-import {Observable} from "rxjs/Observable"
-import {Router} from "@angular/router";
-import {Status} from "../shared/classes/status";
-import {SignUp} from "../shared/classes/sign.up";
-import {setTimeout} from "timers";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {SignUpService} from "../shared/services/sign.up.service";
+import {Component, } from "@angular/core";
+
 
 //declare $ for good old jquery
 declare let $: any;
 
 // set the template url and the selector for the ng powered html tag
 @Component({
-	template: require
-	("./sign-up.component.html"),
-	selector: "sign-up"
+	template: require("./sign-up.component.html"),
+
 })
-export class SignUpComponent implements OnInit {
-
-	//
-	signUpForm: FormGroup;
-
-	signUp: SignUp = new SignUp(null, null, null, null, null);
-	status: Status = null;
+export class SignUpComponent {
 
 
-	constructor(private formBuilder: FormBuilder, private router: Router, private signUpService: SignUpService) {
-	}
+
+	constructor() {}
 
 	ngOnInit(): void {
 
 	}
 
-	createSignUp(): void {
-
-
-	}
 }
