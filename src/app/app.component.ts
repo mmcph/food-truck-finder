f503f6dbdc5ca2dec1506b5543b8f38c0002dfdc
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-06-07 15:17:11 -0600

    added interceptor

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: "food-truck-finder",
+    template: require("./app.component.html")
+})
+
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,19 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./home/home.component";
 import {APP_BASE_HREF} from "@angular/common";
+import {SignUpComponent} from "./sign-up/sign-up.component";
 
 
 export const allAppComponents = [HomeComponent];
 
 export const routes: Routes = [
+    {path: "sign-up", component: SignUpComponent},
     {path: "", component: HomeComponent}
 ];
 
 export const appRoutingProviders: any[] = [
     {provide: APP_BASE_HREF, useValue: window["_base_href"]},
-    HomeComponent
+    {}
 ];
 
 export const routing = RouterModule.forRoot(routes);
