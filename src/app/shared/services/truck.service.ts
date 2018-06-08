7325e8c7cee597c62abac8cfe34e0a470dc74005
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-06-07 23:24:28 -0600

    wrote truck.service.ts

--- src/app/shared/services/truck.service.ts
+++ src/app/shared/services/truck.service.ts
@@ -19,33 +19,43 @@ export class TruckService {
     //reach out to the Truck API and create the truck (POST)
     createTruck(truck : Truck) : Observable<Status> {
         return(this.http.post<Status>(this.truckUrl, truck));
+
     }
 
     // call to the Truck API to grab the truck in question and edit it (PUT)
     editTruck(truck : Truck) : Observable<Status> {
         return(this.http.put<Status>(this.truckUrl + truck.truckId, truck));
+
     }
 
     //reach out to the Truck API and delete the truck in question (DELETE)
     deleteTruck(truckId : string) : Observable<Status> {
         return(this.http.delete<Status>(this.truckUrl + truckId));
+
     }
 
-    // call to the Truck API and get a truck object by its truckId (GET specific truck)
+    // call to the Truck API and get a truck object by its truck id (GET specific truck)
     getTruck(id : string) : Observable<Truck> {
         return(this.http.get<Truck>(this.truckUrl + id));
+
     }
 
-    // call to the Truck API and grab the corresponding truck by its _____? (GET array)
+    // call to the Truck API and get truck objects by a profile id (GET array of trucks)
+    getTruckByTruckProfileId(truckProfileId : string) : Observable<Truck[]> {
+        return(this.http.get<Truck[]>(this.truckUrl + "?truckProfileId=" + truckProfileId));
 
+    }
 
+    // call to the Truck API and get truck objects by isOpen (GET array of trucks)
+    getTruckByTruckIsOpen(truckIsOpen : number) : Observable<Truck[]> {
+        return(this.http.get<Truck[]>(this.truckUrl + "?truckIsOpen=" + truckIsOpen));
 
-    // call to the Truck API and grab the corresponding truck by its _____? (GET array)
+    }
 
+    // call to the Truck API and grab the corresponding trucks and categories by categoryId (GET array)
+    getCategoriesAndTrucksByCategoryId(categoriesAndTrucks : string) : Observable<Truck[]> {
+        return(this.http.get<Truck[]>(this.truckUrl + "?categoryId=" + categoryId));
 
-
-
-
-
+    }
 
 }
