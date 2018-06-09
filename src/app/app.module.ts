ad961ad0200822a81b4a20b8286841a1f3f88c4c
G Cordova <gcordovaone@gmail.com>
2018-06-09 17:06:04 -0600

    added google maps api map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from "./app.component";
 import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {NguiMapModule} from "@ngui/map";
+
 
 const moduleDeclarations = [AppComponent];
 
 @NgModule({
-    imports:      [BrowserModule, HttpClientModule, routing, FormsModule, ReactiveFormsModule],
+    imports:      [BrowserModule, HttpClientModule, routing, FormsModule, ReactiveFormsModule, NguiMapModule.forRoot({apiUrl: 'https://maps.google.com/maps/api/js?key=AIzaSyC3Bc97wjIqDbhwC4gSFz8xM0B4G8PAT-A'})],
     declarations: [...moduleDeclarations, ...allAppComponents],
     bootstrap:    [AppComponent],
     providers:    [...appRoutingProviders]
