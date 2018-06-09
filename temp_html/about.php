5b8e803f8dbd06b3bfdbb4d2f0509a79da65c44a
Manuel Escobar III <manuel.esco.20@gmail.com>
2018-06-09 17:03:27 -0600

    some css styles

---
+++ temp_html/about.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+		<!-- Font Awesome CDN for icons-->
+		<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>
+
+		<!-- Custom CS link -->
+<!--TODO note this: change directory when ever this is put in another directory!-->
+		<link rel="stylesheet" href="./styles/about.css">
+
+		<!-- jQuery, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+		<!--Google Fonts-->
+		<link href="https://fonts.googleapis.com/css?family=Contrail+One|Roboto" rel="stylesheet">
+
+		<!--Actual page begins-->
+		<title>About</title>
+
+	</head>
+
+	<body>
+<div class="centered">
+<h1>ABOUT US</h1>
+		<section class="py-3">
+		<div class="container">
+			<div class="card-body">
+				<div class="row">
+					<div class="col-lg-3 col-sm-6 ">
+						<img class="img-fluid d-block mx-auto mb-4 background-card-img" src="https://www.fillmurray.com/300/300" alt="bill murray">
+					</div>
+					<div>
+						<div class="card mb-4 col-sm-6 background-card fixed">
+							<div class="card-body2">
+								<h5 class="card-title">NAME</h5>
+								<p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the indust</p>
+								<p class="card-text">Personal Web Page<a href="#" role="button"> www.webwhatever.com</a></p>
+<!--								<p><a class="text-favorite" href="#"></a><a class="text-favorite" href="#">www.whatever.com</a></p>-->
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		</section>
+	<!--This Div closes for the site to stay CENTERED!!!-->
+</div>
+	</body>
+</html>
