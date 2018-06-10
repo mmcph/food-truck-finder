08a3c77b25b503c6cff5209da181e6c502e8fdee
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-06-09 19:26:02 -0600

    added truckcategory objects for fake data

--- php/classes/mock-data.php
+++ php/classes/mock-data.php
@@ -14,15 +14,15 @@ require_once("uuid.php");
 //create profile objects and insert them into the data base
 
 //profile 1
-$password = "abc123";
+$password1 = "abc123";
 $hash = password_hash($password, PASSWORD_ARGON2I, ["time_cost" => 384]);
-$profile = new Profile(generateUuidV4(), bin2hex(random_bytes(16)),"g@yahoo.com", $hash, 1, G, Cordova, Gcordova);
+$profile1 = new Profile(generateUuidV4(), bin2hex(random_bytes(16)),"g@yahoo.com", $hash, 1, G, Cordova, Gcordova);
 $profile->insert($pdo);
 echo "first profile";
 var_dump($profile->getProfileId()->toString());
 
 //profile 2
-$password = "abc123";
+$password2 = "abc123";
 $hash = password_hash($password, PASSWORD_ARGON2I, ["time_cost" => 384]);
 $profile2 = new Profile(generateUuidV4(), bin2hex(random_bytes(16)), "yvette@yahoo.com", $hash, "cats",1, Johnson);
 $profile2->insert($pdo);
@@ -30,12 +30,16 @@ echo "second profile";
 var_dump($profile2->getProfileId()->toString());
 
 //profile 3
-$password = "abc123";
+$password3 = "abc123";
 $hash = password_hash($password, PASSWORD_ARGON2I, ["time_cost" => 384]);
 $profile3 = new Profile(generateUuidV4(), bin2hex(random_bytes(16)), "batman@yahoo.com", $hash, "meow", -1, Escobar);
 $profile3->insert($pdo);
 echo "third profile";
 var_dump($profile3->getProfileId()->toString());
+
+
+
+//create truck objects
 
 $truck1 = new Truck(generateUuidV4(), generateUuidV4(), "stuff about this truck", 1, 4.4,1.7, "taco tuesday truck", "505-987-6547", "tacotruck.com");
 
@@ -45,7 +49,15 @@ $truck3 = new Truck(generateUuidV4(), generateUuidV4(), " random stuff about thi
 
 
 
+// create truck category objects
+$truckCategory1 = new TruckCategory(6, generateUuidV4());
+$truckCategory1->insert($pdo);
+echo "first truck-category";
 
+$truckCategory2 = new TruckCategory(7, generateUuidV4());
+$truckCategory2->insert($pdo);
+echo "second truck-category";
 
-
-
+$truckCategory3 = new TruckCategory(8, generateUuidV4());
+$truckCategory3->insert($pdo);
+echo "third truck-category";
