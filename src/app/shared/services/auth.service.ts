ad4e308e0474d8bddc0d14212b01c9a62f49276e
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-06-11 09:01:01 -0600

    added new auth service

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,28 +1,42 @@
-import {JwtHelperService} from "@auth0/angular-jwt";
+// from Rochelle
+import {JwtHelperService} from '@auth0/angular-jwt';
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
-
-
 
 @Injectable()
 
 export class AuthService {
 
+    private token : string = localStorage.getItem("jwt-token");
 
-	private token : string = localStorage.getItem("jwt-token");
+    constructor (
+        private jwtHelperService: JwtHelperService,
+        private http: HttpClient
+    ) {}
 
+    //token : string = this.jwtHelperService.tokenGetter();
+    loggedIn() {
+        if (!this.token) {
+            return false;
+        }
 
+        const tokenExpired: boolean = this.jwtHelperService.isTokenExpired(this.token);
+        return !tokenExpired
+    }
 
-	constructor(private jwtHelperService: JwtHelperService, private http: HttpClient) {}
+    public isAuthenticated(): boolean {
+        const token = localStorage.getItem('jwt-token');
+        // Check whether the token is expired and return true or false
+        return !this.jwtHelperService.isTokenExpired(token);
+    }
 
-	//token : string = this.jwtHelperService.tokenGetter();
-	public isAuthenticated(): boolean {
-		const token = localStorage.getItem('jwt-token');
-		console.log("I lose");
+    decodeJwt() : any {
+        let isLoggedIn : boolean = this.loggedIn();
 
-		console.log(!this.jwtHelperService.isTokenExpired(token));
-		// Check whether the token is expired and return
-		// true or false
-		return !this.jwtHelperService.isTokenExpired(token);
-	}
+        if (!isLoggedIn) {
+            return false;
+        }
+
+        return this.jwtHelperService.decodeToken(this.token);
+    }
 }
